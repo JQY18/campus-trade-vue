6152c1129695081a94d2e295201c61609e690181
Chen jianfeng <2314773181@qq.com>
2024-07-04 21:23:29 +0800

    cjf

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,79 +1,102 @@
 <template>
-  <el-row class="tac">
-    <el-col :span="7">
-      <img :src="require('@/assets/logo3.png')" />
-      <el-menu
-        default-active="2"
-        class="el-menu-vertical-demo"
-        @open="handleOpen"
-        @close="handleClose"
-      >
-        <el-submenu index="1">
-          <template v-slot:title>
-            <i class="el-icon-location"></i>
-            <span>导航一</span>
-          </template>
-          <el-menu-item-group>
-            <template v-slot:title>分组一</template>
-            <el-menu-item index="1-1">选项1</el-menu-item>
-            <el-menu-item index="1-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="1-3">选项3</el-menu-item>
-          </el-menu-item-group>
-          <el-submenu index="1-4">
-            <template v-slot:title>选项4</template>
-            <el-menu-item index="1-4-1">选项1</el-menu-item>
-          </el-submenu>
-        </el-submenu>
-        <el-menu-item index="2">
-          <i class="el-icon-menu"></i>
-          <template v-slot:title>
-            <span>导航二</span>
-          </template>
-        </el-menu-item>
-        <el-menu-item index="3">
-          <i class="el-icon-document"></i>
-          <template v-slot:title>
-            <span>导航三</span>
-          </template>
-        </el-menu-item>
-        <el-menu-item index="4">
-          <i class="el-icon-setting"></i>
-          <template v-slot:title>
-            <span>导航四</span>
-          </template>
-        </el-menu-item>
-      </el-menu>
-    </el-col>
-    <div class="group">
-      <svg viewBox="0 0 24 24" aria-hidden="true" class="icon">
-        <g>
-          <path
-            d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"
-          ></path>
-        </g>
-      </svg>
-      <input class="input" type="search" placeholder="Search" />
-    </div>
+  <el-container>
+    <el-aside >
+      <!-- 侧边导航菜单 -->
+      <el-row class="tac">
+        <el-col :span="12">
+          <img :src="require('@/assets/logo3.png')" />
+          <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
+            <el-submenu index="1">
+              <template v-slot:title>
+                <i class="el-icon-location"></i>
+                <span>导航一</span>
+              </template>
+              <el-menu-item-group>
+                <template v-slot:title>分组一</template>
+                <el-menu-item index="1-1">选项1</el-menu-item>
+                <el-menu-item index="1-2">选项2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="分组2">
+                <el-menu-item index="1-3">选项3</el-menu-item>
+              </el-menu-item-group>
+              <el-submenu index="1-4">
+                <template v-slot:title>选项4</template>
+                <el-menu-item index="1-4-1">选项1</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+            <el-menu-item index="2">
+              <i class="el-icon-menu"></i>
+              <template v-slot:title>
+                <span>导航二</span>
+              </template>
+            </el-menu-item>
+            <el-menu-item index="3">
+              <i class="el-icon-document"></i>
+              <template v-slot:title>
+                <span>导航三</span>
+              </template>
+            </el-menu-item>
+            <el-menu-item index="4">
+              <i class="el-icon-setting"></i>
+              <template v-slot:title>
+                <span>导航四</span>
+              </template>
+            </el-menu-item>
+          </el-menu>
+        </el-col>
+      </el-row>
+    </el-aside>
 
-    <router-link :to="{name: 'login'}"><button>登录</button></router-link>
-    <div class="card">
-<el-col :span="4" v-for="(o, index) in 2" :key="o" :offset="index > 0 ? 2 : 0">
-    <el-card :body-style="{ padding: '0px',margin:'10px'}">
-      <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
-      <div style="padding: 14px;">
-        <span>好吃的汉堡</span>
-        <div class="bottom clearfix">
-          <time class="time">{{ currentDate }}</time>
-          <el-button type="text" class="button">操作按钮</el-button>
+    
+    <!-- 主体部分 -->
+    <el-container>
+      <el-header style="height: 120px">
+        <!-- 头部搜索框 -->
+        <!-- <el-aside>
+       <img :src="require('@/assets/logo3.png')" />
+      </el-aside>
+      <el-main> -->
+        <div class="group">
+          <svg viewBox="0 0 24 24" aria-hidden="true" class="icon">
+            <g>
+              <path
+                d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z">
+              </path>
+            </g>
+          </svg>
+          <input class="input" type="search" placeholder="Search" />
+          
         </div>
-      </div>
-    </el-card>
-  </el-col>
-</div>
-  </el-row>
-
+        <router-link :to="{name:'login'}">
+          <button>登录</button>
+        </router-link>
+        <!-- <router-link :to="{name:'login'}">登录</router-link> -->
+        <!-- <router-link :to="{name:'login'}">登录</router-link> -->
+        <!-- <router-link :to="{name:'login'}">登录</router-link> -->
+        
+     
+      </el-header>
+      <el-main>
+        
+        <div class="card">
+          <el-col :span="4" v-for="(o) in 20" :key="o" :offset="(index%4)==0 ? 0 : 2">
+            <!-- :offset="index > 0 ? 2 : 0" -->
+            <el-card :body-style="{ padding: '0px', margin: '10px' }">
+              <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+                class="image">
+              <div style="padding: 14px;">
+                <span>好吃的汉堡</span>
+                <div class="bottom clearfix">
+                  <time class="time">{{ currentDate }}</time>
+                  <el-button type="text" class="button">操作按钮</el-button>
+                </div>
+              </div>
+            </el-card>
+          </el-col>
+        </div>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 
@@ -85,7 +108,7 @@ export default {
       currentDate: new Date()
     };
   },
-  mounted() {},
+  mounted() { },
   methods: {
     handleOpen(key, keyPath) {
       console.log(key, keyPath);
@@ -101,6 +124,7 @@ export default {
   width: 200px;
   min-height: 400px;
 }
+
 .group {
   display: flex;
   line-height: 40px;
@@ -144,6 +168,7 @@ input:hover {
   width: 1rem;
   height: 1rem;
 }
+
 button {
   padding: 1.3em 3em;
   font-size: 12px;
@@ -171,36 +196,38 @@ button:hover {
 button:active {
   transform: translateY(-1px);
 }
+
 .time {
-    font-size: 13px;
-    color: #999;
-  }
+  font-size: 13px;
+  color: #999;
+}
+
+.bottom {
+  margin-top: 13px;
+  line-height: 12px;
+}
+
+.button {
+  padding: 0;
   
-  .bottom {
-    margin-top: 13px;
-    line-height: 12px;
-  }
+}
 
-  .button {
-    padding: 0;
-    float: right;
-  }
+.image {
+  width: 100%;
+  display: block;
+}
 
-  .image {
-    width: 100%;
-    display: block;
-  }
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
 
-  .clearfix:before,
-  .clearfix:after {
-      display: table;
-      content: "";
-  }
-  
-  .clearfix:after {
-      clear: both
-  }
-  .card{
-    margin-top:20px;
-  }
+.clearfix:after {
+  clear: both
+}
+
+.card {
+  margin-top: 20px;
+}
 </style>
