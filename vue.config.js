ae439bcd9ccb052e55163407b95dfcc7b280e0ac
jqy18 <qiyue@jiang.com>
2024-07-05 09:36:25 +0800

    登陆注册请求

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
-const { defineConfig } = require('@vue/cli-service')
+const {
+  defineConfig
+} = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
+    // 设置端口
+    ,
+  devServer: {
+    port: 8081
+  }
 })
